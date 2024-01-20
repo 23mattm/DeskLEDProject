62d3988f4842294d021054fb29e8f1335caea801
23mattm <mattmara472@gmail.com>
2024-01-19 22:09:30 -0500

    Start LightPin class

--- trying_leds.ino
+++ trying_leds.ino
@@ -1,11 +1,47 @@
+/**
+LED Project
+
+TODO:
+-Implement LightPin class
+-Implement precise timing in loop() function
+-Implement HSV -> RGB and Hue 'autoscroll'
+**/
+
+
 const int SENSOR_PIN = 5;
+
+
+class LightPin {
+  private:
+  int PIN; //the pin number
+  int intens; //the intensity from 0-255
+  public:
+  LightPin(int _pin) {
+    PIN = _pin;
+    intens = 0;
+  }
+
+  //set the intensity
+  void setIntensity(int _intensity) {
+    intens = constrain(_intensity, 0, 255);
+  }
+
+  int getIntensity() {
+    return intens;
+  }
+
+  int getPin() {
+    return PIN;
+  }
+
+
+};
 
 const int RED_PIN = 9;
 const int GREEN_PIN = 10;
 const int BLUE_PIN = 11;
 
 bool debounce = false;
-
 
 
 void setup() {
@@ -24,17 +60,20 @@ void setup() {
 
 
 void loop() {
+  int period = 255;
+
+
   //read motion sensor pin
   bool sensorReading = digitalRead(SENSOR_PIN);
 
   //print the state of the sensor pin:
   //THIS IS SUPER SCUFFED LOL I COULDN'T FIGURE OUT HOW TO PRINT A BOOL
-  if (sensorReading == HIGH) {
-    Serial.write("high");
-  } else {
-    Serial.write("low");
-  }
-  Serial.write("\n");
+  // if (sensorReading == HIGH) {
+  //   Serial.write("high");
+  // } else {
+  //   Serial.write("low");
+  // }
+  // Serial.write("\n");
 
   if (sensorReading == HIGH && !debounce) {
     debounce = true;
