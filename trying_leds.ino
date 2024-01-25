f5e7aa5658650ebf034cfdb74b305c0d6166adb9
23mattm <mattmara472@gmail.com>
2024-01-25 13:27:11 -0500

    RGB conversion doesn't work

--- trying_leds.ino
+++ trying_leds.ino
@@ -13,6 +13,8 @@ const int SENSOR_PIN = 5;
 //Playing with a constant saturation, should give a more pastel-colored light
 const float SATURATION = 0.5;
 const float VALUE = 1.0;
+//The number of seconds it takes for the hue scroller to complete a full rotation
+const float HUE_CYCLE = 5.0;
 
 class LightPin {
 
@@ -56,57 +58,49 @@ class Color {
   */
   void calculateRGB() {
     //Transform sat and val to decimal
-    float S = saturation / 255;
-    float V = value / 255;
+    int H = hue * 360 / 255;
+    float S = (float) saturation / 255.0;
+    float V = (float) value / 255.0;
 
     //Calculate some extra variables.
-    //Formula from rapidtables.com
+    //Formula from rapidtables.com <-- shitty site :-1:
     float C = V * S;
-    float X = C * (1 - abs((hue / 60) % 2 - 1));
+    float X = C * (float) (1 - abs(((H / 60) % 2) - 1));
     float m = V - C;
     float Rp, Gp, Bp;
 
     //Assign RGB prime values
-    hue = (int) 6 * hue / 256;
-    if (hue == 0) {
+    H = 6 * (int) ( H / 360.0);
+    if (H == 0) {
       Rp = C;
       Gp = X;
       Bp = 0.0;
-    } else if (hue == 1) {
+    } else if (H == 1) {
       Rp = X;
       Gp = C;
       Bp = 0.0;
-    } else if (hue == 2) {
+    } else if (H == 2) {
       Rp = 0.0;
       Gp = C;
       Bp = X;
-    } else if (hue == 3) {
+    } else if (H == 3) {
       Rp = 0.0;
       Gp = X;
       Bp = C;
-    } else if (hue == 4) {
+    } else if (H == 4) {
       Rp = X;
       Gp = 0.0;
       Bp = C;
-    } else if (hue == 5) {
+    } else if (H == 5) {
       Rp = C;
       Gp = 0.0;
       Bp = X;
     }
 
     // Assign the RGB values to integers 0-255
-    red = (int) (Rp + m) * 255;
-    green = (int) (Gp + m) * 255;
-    blue = (int) (Bp + m) * 255;
-  }
-
-  /**
-  * Set the HSV for the object
-  */
-  void setHSV(int _hue, int _sat, int _val) {
-    hue = _hue;
-    saturation = _sat;
-    value = _val;
+    red = (int) ((Rp + m) * 255);
+    green = (int) ((Gp + m) * 255);
+    blue = (int) ((Bp + m) * 255);
   }
 
   public:
@@ -133,25 +127,67 @@ class Color {
   static Color fromHSV(float _hue, float _sat, float _val) {
     Color col = Color();
     col.setHSV(
-      (int) _hue * 255, // hue as an int
-      (int) _sat * 255, // sat as an int
-      (int) _val * 255  // val as an int
+      (int) (_hue * 255), // hue as an int
+      (int) (_sat * 255), // sat as an int
+      (int) (_val * 255)  // val as an int
     );
-    col.calculateRGB();
+    return col;
   }
 
+  /**
+  * Set the HSV for the object
+  */
+  void setHSV(int _hue, int _sat, int _val) {
+    hue = _hue;
+    saturation = _sat;
+    value = _val;
+    calculateRGB();
+  }
+
+  int getRed() {
+    return red;
+  }
+
+  int getGreen() {
+    return green;
+  }
+
+  int getBlue() {
+    return blue;
+  }
+
+  int getHue() {
+    return hue;
+  }
+
+  int getSaturation() {
+    return saturation;
+  }
+
+  int getValue() {
+    return value;
+  }
 
   /**
   * Writes the color's properties to the Serial monitor
   */
   void writeOut() {
-    Serial.write("R: " + red);
-    Serial.write(", G: " + green);
-    Serial.write(", B: " + blue);
-    Serial.write('\n');
-    Serial.write("H: " + hue);
-    Serial.write(", S: " + saturation);
-    Serial.write(", V: " + value);
+    Serial.write("\n--------\n");
+    Serial.write("RGB: ");
+    Serial.print(red);
+    Serial.write(", ");
+    Serial.print(green);
+    Serial.write(", ");
+    Serial.print(blue);
+    Serial.write("\n");
+    Serial.write("HSV: ");
+    Serial.print(hue);
+    Serial.write(", ");
+    Serial.print(saturation);
+    Serial.write(", ");
+    Serial.print(value);
+    Serial.write("\n--------");
+
   }
   
 };
@@ -164,6 +200,8 @@ LightPin bluePin = NULL;
 
 //The color object that will control the color of the strip
 Color masterColor;
+float masterHue = 0.0;
+int lastHueUpdate = millis();
 
 void setup() {
   //Setup the sensor pin to read in
@@ -175,22 +213,30 @@ void setup() {
   bluePin = LightPin(11);
 
   //Initialize the masterColor object
-  masterColor = Color::fromHSV(0.0, SATURATION, VALUE);
+  masterColor = Color();
 
   //Begin the serial at 9600 baud.
   Serial.begin(9600);
+  Serial.flush();
 }
-
 
 // Debounce for sensor readings
 bool debounce = false;
 // The main loop for the program
 void loop() {
+
   int period = 255;
+
+  masterColor.setHSV((int) masterHue, (int) (SATURATION * 255.0), (int) (VALUE * 255.0));
+  //Update the master hue
+  float dt = (float) (millis() - lastHueUpdate) / 1000.0;  // Time since last update in seconds
+  masterHue = (int) (masterHue + ((255.0 / HUE_CYCLE) * dt)) % 255;
+  lastHueUpdate = millis();
 
   // Print the master color:
   masterColor.writeOut();
 
+  delay(500);
 
   //read motion sensor pin
   bool sensorReading = digitalRead(SENSOR_PIN);
